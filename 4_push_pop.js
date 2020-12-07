aeefa3ef97ed659b22aa258fa6ceefb617d093dc
jonathankeuls <jonathan.keuls@skola.botkyrka.se>
2020-12-07 16:30:25 +0100

    push_ pop.js

--- 4_push_pop.js
+++ 4_push_pop.js
@@ -1,4 +1,10 @@
-const animals = ['dog', 'cat', 'turtle', 'elephant', 'giraffe']
 
+// pop() tar bort det sista elementet i en array
+const animals = ['dog', 'cat', 'turtle', 'elephant', 'giraffe']
 animals.pop()
 console.log(animals)
+
+// push() lägger till ett element i slutet av arrayen
+animals.push('rhino')
+console.log(animals)
+
