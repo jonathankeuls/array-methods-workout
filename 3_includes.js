bbef71caf19e3070aa53e87ba2add1bbf0fb4e25
jonathankeuls <jonathan.keuls@skola.botkyrka.se>
2020-12-07 16:27:10 +0100

    includes.js

---
+++ 3_includes.js
@@ -0,0 +1,5 @@
+
+// includes() returnerar true eller false beroende på om det givna numret finns i arrayen
+const numbers = [1, 2, 5, 7, 3, 10, 60, -3]
+console.log(numbers.includes(5))
+console.log(numbers.includes(4))
---
+++ 4_push_pop.js
@@ -0,0 +1,4 @@
+const animals = ['dog', 'cat', 'turtle', 'elephant', 'giraffe']
+
+animals.pop()
+console.log(animals)
