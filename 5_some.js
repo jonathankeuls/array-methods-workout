2bd8bada4b7fcb92e34035d9221025498e660293
jonathankeuls <jonathan.keuls@skola.botkyrka.se>
2020-12-12 14:05:44 +0100

    5_some.js

---
+++ 5_some.js
@@ -0,0 +1,9 @@
+
+// some() methoden testar om minst ett element i arrayen passerar den givna functionen
+const numbers = [1, 6, 9, 3, 5, 7, 7, 5, 3, 8, 9, 5, 7]
+
+const isFive = (num) => num === 5
+const isTwo = (num) => num === 2
+
+console.log(numbers.some(isFive))
+console.log(numbers.some(isTwo))
