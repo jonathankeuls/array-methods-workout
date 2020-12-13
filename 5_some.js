88100f6196849bcc693ed2609a34145683910ff0
jonathankeuls <jonathan.keuls@skola.botkyrka.se>
2020-12-13 12:16:40 +0100

    7_map.js

--- 5_some.js
+++ 5_some.js
@@ -1,5 +1,5 @@
 
-// some() methoden testar om minst ett element i arrayen passerar den givna functionen
+// some() metoden testar om minst ett element i arrayen passerar den givna functionen
 const numbers = [1, 6, 9, 3, 5, 7, 7, 5, 3, 8, 9, 5, 7]
 
 const isFive = (num) => num === 5
---
+++ 7_map.js
@@ -0,0 +1,7 @@
+
+// map() metoden tar en given array och skickar tillbaka en ny array och gör om varje element utefter functionen
+const myArray = [3, 6, 8, 4]
+
+const myArray2 = myArray.map(x => x * 2)
+
+console.log(myArray2)
