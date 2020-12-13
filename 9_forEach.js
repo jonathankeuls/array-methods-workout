4ca5290f53708783de29a0c4a527bd2b8dca33a7
jonathankeuls <jonathan.keuls@skola.botkyrka.se>
2020-12-13 12:40:25 +0100

    9_forEach

--- 9_forEach.js
+++ 9_forEach.js
@@ -1,3 +1,5 @@
+
+// metoden forEach() kör en given function en gång för varje element
 const cars = ['Volvo', 'Audi', 'Mercedes', 'Volkswagen', 'Kia']
 
 cars.forEach(element => console.log(`Jag gillar ${element}`))
