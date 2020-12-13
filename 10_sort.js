e0d34a212caf63bcdac7d757b69da9022fd5c550
jonathankeuls <jonathan.keuls@skola.botkyrka.se>
2020-12-13 18:06:01 +0100

    10_sort.js

---
+++ 10_sort.js
@@ -0,0 +1,7 @@
+
+// sort() sorterar en array på olika sätt. I detta exempel sorteras "numbers" efter nummerordning
+const numbers = [5, 8, 1, 3, 2, 6, 9, 7, 0, 4]
+
+numbers.sort()
+
+console.log(numbers) // [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
